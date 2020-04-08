5e8bd27e64cea33e84efe9c1a4955150c9fcefcf
Tim Fish <tim@timfish.uk>
2020-04-08 13:11:30 +0100

    Remove async import()

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,15 +1,13 @@
 import { bootstrap } from 'aurelia-bootstrapper';
 import { Aurelia } from 'aurelia-framework';
 import { App } from './app';
-// import { configure as configureModule } from '@timfish/module';
+import { configure as configureModule } from '@timfish/module';
 
 bootstrap(async (aurelia: Aurelia) => {
-  let mod = await import('@timfish/module');
-
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
-    .feature(mod.configure);
+    .feature(configureModule);
 
   const root = document.getElementById('app-host') as Element;
 
